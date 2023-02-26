1dfd6749b79dc2525c1ac470f6d74ae2270b38dc
Kauan Cleuton <kauancleuton733@gmail.com>
2023-02-26 14:59:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+const randomNumber = Math.round(Math.random()* 10)
+let xAttempts = 1
+
+//Eventos
+btnTry.addEventListener('click', function(event) {
+  event.preventDefault()
+  const inputNumber = document.querySelector('#inputNumber')
+  
+  if(Number(inputNumber.value) == randomNumber){
+    toggleScreen()
+    document.querySelector('.screen2 h1').innerHTML = `Acertou em ${xAttempts} tentativas`
+  }
+  inputNumber.value = ""
+  xAttempts++
+})
+
+btnReset.addEventListener('click', function(){
+  toggleScreen()
+  xAttempts = 1
+})
+
+//Funções
+function toggleScreen(){
+  screen1.classList.toggle('hide')
+  screen2.classList.toggle('hide')
+}
